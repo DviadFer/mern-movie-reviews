71bda5a7d8adce0491bc8c5b7303c4072e3ab29a
DviadFer <diegoviadfer@gmail.com>
2023-05-01 19:59:35 +0200

    Create single movie component and its styles. Fixed navbar logo with link to home.

--- frontend/src/components/movie-single.jsx
+++ frontend/src/components/movie-single.jsx
@@ -1,9 +1,94 @@
-function MovieSingle() {
+import {useState, useEffect} from 'react'
+import MovieDataService from '../services/movies'
+import { Link } from 'react-router-dom'
+import styles from './movie-single.module.scss'
+import { FaStar, FaChevronRight } from 'react-icons/fa'
+
+const MovieSingle = props => {
+    //Definimos el objeto movie para rellenar despues de usar el servicio movies.js
+    const [movie, setMovie] = useState({
+        title: '',
+        fullplt: '',
+        imdb : {
+            rating: null
+        },
+        genres: [],
+        cast: [],
+        reviews: []
+    })
+
+    //Definimos la funcion que usa get(id) del servicio como reslucion de la promise. Se activará en el use Effect
+    const getMovie = id =>{
+        MovieDataService.get(id)
+        .then(response => {
+            setMovie(response.data)
+            console.log(response.data)
+        })
+        .catch(e =>{
+            console.log(e)
+        })
+    }
+
+    //Formato a la fecha
+    const formatDate = (date) => {
+        return date.split('T')[0]
+    }
+
+    useEffect(()=>{
+        //props.match.params es la sintáxis que usamos para acceder a los parmaetros dinámicos asociados desde la url
+        getMovie(props.match.params.id)
+    },[props.match.params.id]) //[] si esta vacio hace que solo se active una vez, como no es así, se activará cada vez que el id cambie
+
     return (
-        <div>
-            Movie
+        <>
+        <div className={styles.movieContainer}>
+            <div className={styles.imageWrapper}>
+                <div className={`${movie.rated ? styles.rating : ''}`}>{movie.rated}</div>
+                <img className={styles.image} src={`${movie.poster}`} alt="movie-img" onError={({ currentTarget }) => {
+                    currentTarget.onerror = null // prevents looping
+                    currentTarget.src="/image-not-found.webp"
+                }}/>
+            </div>
+            <div className={styles.movieInfo}>
+                <h1>{movie.title}</h1>
+                <div className={styles.meta}>
+                    <div className={styles.imdb}><FaStar/><span>{movie.imdb.rating}</span></div>
+                    <div className={styles.divisor}/>
+                    <div>{movie.released ? formatDate(movie.released) : 'UNDEFINED'}</div>
+                    <div className={styles.divisor}/>
+                    <div className={styles.genres}>
+                        {movie.genres.map( (genre, index) => {
+                            return (
+                                <span key={`${genre}-${index}`}>
+                                    {genre}{index !== (movie.genres.length - 1) ? ", " : ""}
+                                </span>
+                            )
+                        })}
+                    </div>
+                </div>
+                <p className={styles.fullPlot}>{movie.fullplot}</p>
+                <h3>Featured Cast</h3>
+                <div className={styles.cast}>
+                    {movie.cast.map( (cast, index) => {
+                        return (
+                            <div key={`${cast}-${index}`}>{cast}</div>
+                        )
+                    })}
+                </div>
+                {/*true se cambiará por user.prop*/}
+                {true &&
+                    <Link className={styles.addReview} to={"/movies/" + props.match.params.id + "/review"}>
+                        <span>Add Review</span>
+                        <FaChevronRight />
+                    </Link>
+                }                
+            </div>
         </div>
-    )
+        <div className={styles.reviewContainer}>
+            <h2>Reviews</h2>
+        </div>
+        </>
+    );
 }
 
 export default MovieSingle
